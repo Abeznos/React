3702ba2e2f932f0b65e8bd51dace11a3a2a2ebfd
Abeznos <34938320+Abeznos@users.noreply.github.com>
2019-02-13 17:13:33 +0300

    Добавил класс Manager

--- task_3.js
+++ task_3.js
@@ -20,3 +20,28 @@ class Employee extends Human {
         return `${super.displayInfo()}, ${this.salary}, ${this.department}`;
     }
 }
+
+class Manager extends Employee {
+    constructor (name, age, dateOfBirth, salary, department) {
+        super(name, age, dateOfBirth, salary, department);
+        this.listOfEmployees = ['Ivan', 'Oleg', 'Vladimir'];
+    }
+    
+    removeEmployee(person) {
+        this.listOfEmployees.forEach(key => {
+            if (key === person) {
+                this.listOfEmployees
+                    .unshift(...this.listOfEmployees.splice(this.listOfEmployees.indexOf(key), 1));
+                this.listOfEmployees.shift();
+                console.log(this.listOfEmployees);
+            }
+        });
+    }
+    
+    addEmployee(person) {
+        this.listOfEmployees.push(person);
+        console.log(this.listOfEmployees);
+    }
+}
+let ivan = new Manager('Иван', 24, '04.08.1995', 10000, 'Marketing');
+ivan.removeEmployee('Oleg');
